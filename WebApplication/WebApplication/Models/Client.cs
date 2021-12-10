08ae7ba7b993b28344be8757b2ffcde18d9b5d09
Cyanic <studnicki36@gmail.com>
2021-12-10 20:07:37 +0100

    Creating Client and Storage models

---
+++ WebApplication/WebApplication/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [Required]
+        public string PhoneNum { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ WebApplication/WebApplication/Models/Storage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Storage
+    {
+        [Key]
+        public int StorageId { get; set; }
+        public int StorageNum { get; set; }
+        [Required]
+        public string PlateNum { get; set; }
+        [Required]
+        public string TireDescription { get; set; }
+        [Required]
+        public int TireNum { get; set; }
+        public string RimDescription { get; set; }
+        public int RimNum { get; set; }
+        public string OverallDescription { get; set; }
+
+        //Foreign key for Client
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+    }
+}
